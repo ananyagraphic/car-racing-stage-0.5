209c502710ac4615ced898bf4fd42db13c3ec7c6
ananyagraphic <75197504+ananyagraphic@users.noreply.github.com>
2021-01-27 18:51:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# car-racing-stage-0.5
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,11 @@
+var Canvas
+var gameState=0
+var playerCount
+var database
+var form,player,game
+
+function setup() {
+ Canvas=createCanvas(400,400) 
+ database=firebase.database()  
+}
+function draw(){}
